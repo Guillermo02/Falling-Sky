3b2727c5e797260d0587ac8963ff23bbbf1b2aec
Guillermo02 <guigokuznietz@gmail.com>
2020-11-22 01:09:56 -0300

    New kirby modes....and fixed HITBOXES

---
+++ Kirby code.py
@@ -0,0 +1,25 @@
+#-- ITS KIRBYS TIME --#
+#Kirby Azul
+kirby b parado_img = pygame.image.load('img/Kirby b/Kirby b parado.png').convert_alpha()
+kirby b parado_img = pygame.transform.scale(inim_img, (140, 140))
+kirby b correndo_img = pygame.image.load('img/Kirby b/Kirby b correndo.png').convert_alpha()
+kirby b correndo_img = pygame.transform.scale(inim_img, (140, 140))
+kirby b caindo_img = pygame.image.load('img/Kirby b/Kirby b caindo.png').convert_alpha()
+kirby b caindo_img = pygame.transform.scale(inim_img, (140, 140))
+kb = [kirby b parado_img, kirby b correndo_img, kirby b caindo_img]
+#Kirby Amarelo
+kirby y parado_img = pygame.image.load('img/Kirby y/Kirby y parado.png').convert_alpha()
+kirby y parado_img = pygame.transform.scale(inim_img, (140, 140))
+kirby y correndo_img = pygame.image.load('img/Kirby y/Kirby y correndo.png').convert_alpha()
+kirby y correndo_img = pygame.transform.scale(inim_img, (140, 140))
+kirby y caindo_img = pygame.image.load('img/Kirby y/Kirby y caindo.png').convert_alpha()
+kirby y caindo_img = pygame.transform.scale(inim_img, (140, 140))
+ky = [kirby y parado_img, kirby y correndo_img, kirby y caindo_img]
+#Kirby Verde
+kirby g parado_img = pygame.image.load('img/Kirby g/Kirby g parado.png').convert_alpha()
+kirby g parado_img = pygame.transform.scale(inim_img, (140, 140))
+kirby g correndo_img = pygame.image.load('img/Kirby g/Kirby g correndo.png').convert_alpha()
+kirby g correndo_img = pygame.transform.scale(inim_img, (140, 140))
+kirby g caindo_img = pygame.image.load('img/Kirby g/Kirby g caindo.png').convert_alpha()
+kirby g caindo_img = pygame.transform.scale(inim_img, (140, 140))
+kg = [kirby g parado_img, kirby g correndo_img, kirby g caindo_img]
