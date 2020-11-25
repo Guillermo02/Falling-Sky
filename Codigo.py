08be438f1f8a55f703f061e5096820feaac6efaa
Guillermo02 <guigokuznietz@gmail.com>
2020-11-25 18:07:05 -0300

    Jogo funcionando com alguns pontos faltando

--- Codigo.py
+++ Codigo.py
@@ -1,7 +1,9 @@
 import pygame
 import random
+#import Kirbys   ##Possibilidade de kerbys extras
 
 pygame.init()
+#pygame.mixer.init()   ##MUSICA
 
 # ----- Gera tela principal
 WIDTH = 700
@@ -11,35 +13,42 @@ pygame.display.set_caption('Falling-Sky')
 
 
 # ----- Inicia assets
-#TAMANHO DO TRECO CAINDO
-#TAMANHO DO TRECO CAINDO
+##Os nomes das variáveis está em uma mistura de português e inglês##
+#assets jogador
 JOG_WIDTH = 90
-JOG_HEIGHT = 80
-font = pygame.font.SysFont(None, 48) #FONTE DE LETRA UTILIZADA
-background = pygame.image.load('img/Fundo.png').convert()
-#CODIGO DO TRECO CAINDO
-#CODIGO DO TRECO CAINDO
-jog_img = pygame.image.load('img/Kirby parado.png').convert_alpha()
+JOG_HEIGHT = 70
+jog_img = pygame.image.load('img/kirby parado.png').convert_alpha()
 jog_img = pygame.transform.scale(jog_img, (JOG_WIDTH, JOG_HEIGHT))
+#assets inimigo
 inim_img = pygame.image.load('img/roda da morte.png').convert_alpha()
-inim_img = pygame.transform.scale(inim_img, (140, 140))
-poder_img = pygame.image.load('img/gemas/hab b.png').convert_alpha()
-poder_img = pygame.transform.scale(poder_img, (20,20))
-
- # Redimensiona o fundo   
+inim_img = pygame.transform.scale(inim_img, (60, 60))
+#assets gemas
+gemab_img = pygame.image.load('img/gemas/hab b.png').convert_alpha()
+gemay_img = pygame.image.load('img/gemas/hab y.png').convert_alpha()
+gemag_img = pygame.image.load('img/gemas/hab g.png').convert_alpha()
+t_gemas = [gemab_img, gemay_img, gemag_img]
+#assets background
+background = pygame.image.load('img/Fundo.png').convert()
 background = pygame.transform.scale(background, (700, 620))
 background_rect = background.get_rect()
+#assets fonte de texto
+score_font = pygame.font.Font('font/PressStart2P.ttf', 28)
 
-# Define a aceleração da gravidade
-GRAVITY = 6
+# Carrega os sons do jogo
+#pygame.mixer.music.load('snd/Ketsa_-_10_-_wallow.mp3')
+#pygame.mixer.music.set_volume(0.4)
+
 # Define estados possíveis do jogador
 STILL = 0
 JUMPING = 1
-FALLING = 2
+
+# Define valores iniciais
+GRAVITY = 6
+PONTOS = 0
+VIDAS = 3
 
 class jogador(pygame.sprite.Sprite):
-    def __init__(self, jog_img):
-        # Construtor da classe mãe (Sprite).
+    def __init__(self, jog_img, VIDAS):
         pygame.sprite.Sprite.__init__(self)
 
         # Define estado atual
@@ -51,19 +60,39 @@ class jogador(pygame.sprite.Sprite):
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
         self.speedy = 0
+        self.lifes = VIDAS
     
     def update(self):
         #Movimentação em y
         self.speedy += GRAVITY
-        if self.speedy > 0:
-            self.state = FALLING
-        # Atualiza a posição y
+        #Atualiza a posição y
         self.rect.y += self.speedy
-
-        #Movimentação em x
+        #Atualiza a posição x
         self.rect.x += self.speedx
+
+        ## Mudança de sprite para cada situação que estiver ##
+        #Se estiver no ar: kirby no ar
+        if self.rect.bottom < HEIGHT-10:
+            self.image = pygame.image.load('img/kirby saltando.png').convert_alpha()
+        #Se estiver parado: kirby parado
+        if self.rect.bottom > HEIGHT-9 and self.speedx==0:
+            self.image = pygame.image.load('img/kirby parado.png').convert_alpha()
+        #Se estiver correndo para a direita: kirby direita
+        if self.speedx > 0:
+            self.image = pygame.image.load('img/kirby correndo d.png').convert_alpha()
+        #Se estiver correndo para a esquerda: kirby esquerda
+        if self.speedx < 0:
+            self.image = pygame.image.load('img/kirby correndo e.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (JOG_WIDTH, JOG_HEIGHT))
+
+
+        #Se jogador colidiu com algum inimigo
+        collisions = pygame.sprite.spritecollide(self, collide_destruidor, False)
+        #Perde uma vida
+        for collision in collisions:
+            self.lifes -= 1
         
-        # Corrige a posição para não sair da janela
+        #Corrige a posição para não sair da janela
         if self.rect.left < 0:
             self.rect.left = 0
         if self.rect.right >= WIDTH:
@@ -72,108 +101,159 @@ class jogador(pygame.sprite.Sprite):
             self.rect.bottom = HEIGHT
             self.speedy = 0
             self.state = STILL
-            
-            
-    # Método que faz o personagem pular
+
+       
+    #Método que faz o personagem pular
     def jump(self):
-        # Só pode pular se ainda não estiver pulando ou caindo
+        #Só pode pular se ainda não estiver pulando ou caindo
         if self.state == STILL:
             self.speedy -= 50
             self.state = JUMPING
-
+        
 class inimigo(pygame.sprite.Sprite):
     def __init__(self, inim_img):
-        # Construtor da classe mãe (Sprite).
         pygame.sprite.Sprite.__init__(self)
-
-
+        
         # Define estado atual
         self.image = inim_img
         self.rect = self.image.get_rect()
         self.rect.centerx = -100
-        self.rect.centery = WIDTH - 500
+        self.rect.centery = WIDTH - 100
         self.speedx = 0
         self.speedy = 0
     
     def update(self):
-        self.rect.x += 6
+        self.rect.x += 4
+        if self.rect.left > WIDTH:
+            self.rect.right = -100
 
-        if self.rect.centerx > WIDTH:
-            self.rect.centerx = -100
-
-class poder():
-    def __init__(self, poder_img):
-        # Construtor da classe mãe (Sprite).
+class poder(pygame.sprite.Sprite):
+    def __init__(self, gema_img):
         pygame.sprite.Sprite.__init__(self)
-
-        self.image = poder_img
+        
+        self.image = gema_img
         self.rect = self.image.get_rect()
-        self.rect.centerx = random.randint(10,690)
-        self.rect.centery =random.randint (520,WIDTH)
-        self.speedx = 0
+        self.rect.centerx = random.randint(30,670)
+        self.rect.centery = random.randint (WIDTH-300,WIDTH-200)
+        self.speedx = 0 
         self.speedy = 0
 
-    #def update (self):
-
-
-
-
 game = True
-# Variável para o ajuste de velocidade
-clock = pygame.time.Clock()
+#Cria um relógio que conta o tempo em jogo
+tempo = pygame.time.Clock()
 FPS = 30
 
-# Criando um grupo de TRECOS CAINDO
+#Gemas aleatórias que aparecem
+gema = random.choice(t_gemas)
+gema_img = pygame.transform.scale(gema, (40,40))
+
+#Cria grupos com as sprites e collides
 all_sprites = pygame.sprite.Group()
-#CODIGO DAS SPRITES DO TRECO CAINDO all_XXX = pygame.sprite.Group()
-# Criando o jogador
-player = jogador(jog_img)
+collide_destruidor = pygame.sprite.Group()
+collide_gema = pygame.sprite.Group()
+# Criando o jogador, inimigo e gemas
+player = jogador(jog_img, VIDAS)
 destruidor = inimigo(inim_img)
-#podersinho = poder(poder_img)
+gema_ponto = poder(gema_img)
+#Adicionando sprites em uma variável global
 all_sprites.add(player)
 all_sprites.add(destruidor)
-#all_sprites.add(podersinho)
-# Criando os TRECOS CAINDO
-#for i in range(8):
-    #meteor = Meteor(meteor_img)
-    #all_sprites.add(meteor)
-    #all_meteors.add(meteor)
+collide_destruidor.add(destruidor)
+for i in range(5):
+    gema_ponto = poder(gema_img)
+    all_sprites.add(gema_ponto)
+    collide_gema.add(gema_ponto)
+
 
 # ===== Loop principal =====
+#pygame.mixer.music.play(loops=-1) ##MUSICA
 while game:
-    clock.tick(FPS)
-    #t += 5
+    if VIDAS > 0:
+        tempo.tick(FPS)
+        
+        # ----- Trata eventos
+        for event in pygame.event.get():
+            # ----- Verifica consequências
+            if event.type == pygame.QUIT:
+                game = False
+            # Verifica se apertou alguma tecla.
+            if event.type == pygame.KEYDOWN:
+                # Dependendo da tecla, altera a velocidade.
+                if event.key == pygame.K_LEFT:
+                    player.speedx -= 9
+                if event.key == pygame.K_RIGHT:
+                    player.speedx += 9
+                elif event.key == pygame.K_UP:
+                    player.jump()
+            # Verifica se soltou alguma tecla.
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    player.image = pygame.image.load('img/kirby parado.png').convert_alpha()
+                    player.speedx = 0
+        
+        hits = pygame.sprite.spritecollide(player, collide_destruidor, True, pygame.sprite.collide_mask)
+        if len(hits) > 0:
+            VIDAS -= 1
+            destruidor.rect.x = -200
+            all_sprites.add(destruidor)
+            collide_destruidor.add(destruidor)
+        contato = pygame.sprite.spritecollide(player, collide_gema, True)
+        
+        #Pontos 
+        if len(contato)>0:
+            if gema == gemay_img or gema == gemag_img:
+                PONTOS += 10
+            elif gema == gemab_img:
+                PONTOS += 15
+        
+        for gema in contato:
+            gema = random.choice(t_gemas)
+            gema_img = pygame.transform.scale(gema, (40,40))
+            gema_ponto = poder(gema_img)
+            all_sprites.add(gema_ponto)
+            collide_gema.add(gema_ponto)
 
-    # ----- Trata eventos
-    for event in pygame.event.get():
-        # ----- Verifica consequências
-        if event.type == pygame.QUIT:
-            game = False
-        # Verifica se apertou alguma tecla.
-        if event.type == pygame.KEYDOWN:
-            # Dependendo da tecla, altera a velocidade.
-            if event.key == pygame.K_LEFT:
-                player.speedx -= 9
-            if event.key == pygame.K_RIGHT:
-                player.speedx += 9
-            elif event.key == pygame.K_UP:
-                player.jump()
-        # Verifica se soltou alguma tecla.
-        if event.type == pygame.KEYUP:
-            # Dependendo da tecla, altera a velocidade.
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                player.speedx = 0
-            
+        if PONTOS % 1000 == 0 and VIDAS<3:
+            VIDAS += 1
+        # ----- Atualiza estado do jogo
+        # Atualizando a posição dos meteoros
+        all_sprites.update()
+        collide_destruidor.update()
+        collide_gema.update()
 
-    # ----- Atualiza estado do jogo
-    # Atualizando a posição dos meteoros
-    all_sprites.update()
+        # ----- Gera saídas
+        window.fill((0, 0, 0))  # Preenche com a cor branca
+        window.blit(background, (0, 0))
+        # Desenhando as sprites
+        all_sprites.draw(window)
+        
+        # Desenhando o score
+        text_surface = score_font.render("{:06d}".format(PONTOS), True, (255, 0, 200))
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (90,  10)
+        window.blit(text_surface, text_rect)
 
-    # ----- Gera saídas
-    window.fill((0, 0, 0))  # Preenche com a cor branca
-    window.blit(background, (0, 0))
-    # Desenhando meteoros
-    all_sprites.draw(window)
+        # Desenhando as vidas
+        text_surface = score_font.render(chr(9829) * VIDAS, True, (255, 0, 0))
+        text_rect = text_surface.get_rect()
+        text_rect.bottomleft = (10, HEIGHT - 10)
+        window.blit(text_surface, text_rect)
+
+
+    
+    else:
+        window.fill((0, 0, 0))
+        text_surface = score_font.render("Você Perdeu :(", True, (255, 0, 0))
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (WIDTH/2, HEIGHT/2)
+        window.blit(text_surface, text_rect)
+
+        for event in pygame.event.get():
+            # ----- Verifica consequências
+            if event.type == pygame.QUIT:
+                game = False
+            if event.type == pygame.KEYDOWN:
+                game = False
 
     pygame.display.update()  # Mostra o novo frame para o jogador
 
---
+++ Kirbys.py
@@ -0,0 +1,16 @@
+import pygame
+pygame.init()
+
+##Kirbys##
+#Kirby Azul
+kirbybparado_img   = pygame.image.load('img/Kirby b/Kirby b parado.png').convert_alpha()
+kirbybcorrendo_img = pygame.image.load('img/Kirby b/Kirby b correndo.png').convert_alpha()
+kirbybcaindo_img   = pygame.image.load('img/Kirby b/Kirby b caindo.png').convert_alpha()
+#Kirby Amarelo
+kirbyyparado_img   = pygame.image.load('img/Kirby y/Kirby y parado.png').convert_alpha()
+kirbyycorrendo_img = pygame.image.load('img/Kirby y/Kirby y correndo.png').convert_alpha()
+kirbyycaindo_img   = pygame.image.load('img/Kirby y/Kirby y caindo.png').convert_alpha()
+#Kirby Verde
+kirbygparado_img   = pygame.image.load('img/Kirby g/Kirby g parado.png').convert_alpha()
+kirbygcorrendo_img = pygame.image.load('img/Kirby g/Kirby g correndo.png').convert_alpha()
+kirbygcaindo_img   = pygame.image.load('img/Kirby g/Kirby g caindo.png').convert_alpha()
